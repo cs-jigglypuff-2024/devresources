26795248fb69620568634c34b9098ddca208c98c
Dennis Corsi <denniscorsi@gmail.com>
2024-01-02 15:04:14 -0500

    Add queries module

--- server/queries.js
+++ server/queries.js
@@ -1,10 +1,20 @@
+const db = require('./model.js');
+
 const queries = {};
 
 // Input: a search query string
-// Output: returns array of ids of 
-queries.search = (str) => {
+// Output: returns array of ids of resources with the string in the title
+queries.search = async (str) => {
+  const query = `
+    SELECT id
+    FROM resources
+    WHERE title LIKE '%$1%';
+  `;
+  values = [str];
 
-
-}
+  const result = await db.query(query, values);
+  const ids = result.rows[0];
+  console.log('query restult:', ids);
+};
 
 module.exports = queries;
