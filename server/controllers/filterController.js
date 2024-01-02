05e3c6d17bf674ed62cdd9e0ef77865d332058e8
Dennis Corsi <denniscorsi@gmail.com>
2024-01-02 18:36:29 -0500

    Add routing for resources

--- server/controllers/filterController.js
+++ server/controllers/filterController.js
@@ -13,4 +13,18 @@ filterController.search = (req, res, next) => {
   });
 };
 
+filterController.getResources = async (req, res, next) => {
+  const { ids } = req.body;
+  const resources = [];
+  // For each resource id, query the databse and add the returned object to the cards object
+  for (const id of ids) {
+    // TODO: update name based on Jackson's naming
+    let resource = await query.getResourceById(id);
+    resources.push(resource);
+  }
+
+  res.locals.resources = resources;
+  return next();
+};
+
 module.exports = filterController;
