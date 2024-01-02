a148444be39d3a2b218341e41ab53ed935d4b2f7
Dennis Corsi <denniscorsi@gmail.com>
2024-01-02 13:54:29 -0500

    add file skeleton

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+// route handler to respond with main app
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../app/index.html'));
+});
+
+// catch-all route handler for any requests to an unknown route
+app.use('*', (req, res) => {
+  return res.sendStatus(404);
+});
+
+// global error handling middleware
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+// initialize port listening
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    host: '0.0.0.0',
+    // port: 8080 by default
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: true,
+    proxy: {
+      // list every endpoint
+      // '/endpoint': 'http://localhost:3000'
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /.(css|scss)$/,
+        // exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'], // order reads right to left (turns sass files to css to style string)
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './app/index.html'),
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'], // these files can be imported without specifying extension
+  },
+};
