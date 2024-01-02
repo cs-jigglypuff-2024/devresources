4bc022f83ed92c473771dde16c97d4a2020f8d32
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2024-01-02 15:46:30 -0500

    GO FLIGHT

--- server/queries.js
+++ server/queries.js
@@ -1,5 +1,34 @@
 const db = require('./model.js');
 
+/* These are the queries used to create our tables:
+
+Used to create users table:
+
+	“CREATE TABLE users (id biting, username varchar, PRIMARY KEY (id))
+
+Used to create folders table with ‘user_id’ as ‘id’ from users table:
+
+	“CREATE TABLE folders (id bigint, name varchar(255), PRIMARY KEY (id), user_id bigint, FOREIGN KEY (user_id) REFERENCES users(id))”
+
+Used to create user_tag_join table with user_id as ‘id’ from users table and tag_id as ‘id’ from tags table:
+
+  “CREATE TABLE user_tag_join (user_id bigint, tag_id bigint, FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (tag_id) REFERENCES tags(id), PRIMARY KEY (user_id, tag_id))”
+
+Used to create resources table:
+
+	“CREATE TABLE resources (id bigint, title varchar, url varchar, description varchar, clicks bigint, date_added date, PRIMARY KEY (id))”
+
+Used to create resource_tag_join table with resource_id as ‘id’ from resources table and tag_id as ‘id’ from tags table:
+
+	“CREATE TABLE resource_tag_join (resource_id bigint, tag_id bigint, FOREIGN KEY (resource_id) REFERENCES resources(id), FOREIGN KEY (tag_id) REFERENCES tags(id), PRIMARY KEY (resource_id, tag_id))”
+
+Used to create folder_resource_join with folder_id as ‘id’ from folders and resource_id as ‘id’ from resources:
+
+	“CREATE TABLE folder_resource_join (id bigint, folder_id bigint, resource_id bigint, PRIMARY KEY(bigint), FOREIGN KEY (folder_id) REFERENCES folders(id), FOREIGN KEY (resource_id) REFERENCES resources(id))”
+  
+*/
+
+
 const queries = {};
 
 // Input: a search query string
