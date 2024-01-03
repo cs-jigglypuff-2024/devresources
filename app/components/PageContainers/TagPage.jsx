368613fd67786143e1091421b8d083f1647de007
Dennis Corsi <denniscorsi@gmail.com>
2024-01-03 10:47:55 -0500

    Setup TagPage component

--- app/components/PageContainers/TagPage.jsx
+++ app/components/PageContainers/TagPage.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import RowView from '../ViewComponents/RowView';
 
 function TagPage() {
+  const tag = useLocation().state.tag;
+  const newCards = [];
+  const trendingCards = [];
+
+  useEffect(() => {
+    // get new
+    
+    // get trending
+  }, [tag]);
 
   //fetch queries here, prop drill all returns into appropriate row views below.
   return (
     <div>
-      TagPage, row view below:
-    <RowView></RowView>
+      <h1>{tag}</h1>
+      <RowView cards={newCards} title='NEW' />
+      <RowView cards={trendingCards} title='TRENDING' />
     </div>
-  )
+  );
 }
 
-export default TagPage
+export default TagPage;
--- app/components/Tag.jsx
+++ app/components/Tag.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './views_styles.scss';
 
 function Tag({ tag }) {
+  const navigate = useNavigate();
+
   const clickHandler = () => {
     console.log('clicked', tag);
-    
+    navigate('/tagPage', { state: { tag } });
   };
 
   return (
