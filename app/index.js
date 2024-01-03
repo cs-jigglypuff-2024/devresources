39af52330995f821ea4bf61f2972bd88b31b72d8
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2024-01-03 11:09:09 -0500

    Basic Setup

--- app/index.js
+++ app/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
-import { BrowserRouter } from 'react-router-dom';
 
 //wrapping app in react router
 const root = createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
