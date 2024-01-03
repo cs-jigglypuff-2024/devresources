4d00386bc8fb52a2fee3ba61f67f4932c6bc516c
Dennis Corsi <denniscorsi@gmail.com>
2024-01-03 14:55:35 -0500

    Fatch all tags

--- app/components/CardComponents/TagsView.jsx
+++ app/components/CardComponents/TagsView.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import Tag from '../Tag';
 import '../views_styles.scss';
 
-function TagsView(props) {
+function TagsView() {
+  let tags = [];
+
+  // Get list of all tags
+  fetch('/getTags')
+    .then((res) => res.json())
+    .then((tagsArr) => {
+      console.log('TAGS:', tagsArr);
+      tags = tagsArr;
+    })
+    .catch((err) => {});
+
   //map input array from props (props.tagList) to capsule UI elements with links to tagPage view
+  const Tags = tags.map((tag) => <Tag tag={tag} />);
   //for now, two mock tags as paragraphs
-  return (
-    <div className='tags'>
-      <p>tag1</p>
-      <p>tag2</p>
-    </div>
-  );
+  return <div className='tags'>{Tags}</div>;
 }
 
 export default TagsView;
